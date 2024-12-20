7a5623ece8d539905973c320c4a5abd6f2a741f5
Su Kwon <skwon2@c3r3p6.hive.fi>
2024-12-20 16:29:21 +0200

    ex01

---
+++ ex01/Fixed.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skwon2 <skwon2@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/18 11:06:18 by skwon2            #+#    #+#             */
+/*   Updated: 2024/12/20 14:25:19 by skwon2           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <cmath>
+# include <iostream>
+
+
+class Fixed{
+        private:
+            int _fixedPoint;
+            static const int fractional_bits = 8;
+        public:
+            Fixed();
+            Fixed(const int intNum);
+            Fixed(const float floatNum);
+            Fixed(const Fixed& oldFixed); // copy constructor
+            ~Fixed();
+            Fixed& operator=(const Fixed& oldFixed);//copy assignment operator overload.
+           
+            // int getRawBits( void ) const;
+            // void setRawBits( int const raw );
+           float toFloat( void ) const;
+           int toInt( void ) const;
+};
+
+//not related in maniplating  fixed point
+// stream insertion related.
+ 
+std::ostream& operator<<(std::ostream& out, const Fixed& object);//copy assignment operator overload.
+#endif
